19a3b572e2c40dd48b7f851a451ff6e491423760
pengchangxi <792447592@qq.com>
2018-03-06 14:56:13 +0800

    实现头像上传剪裁

---
+++ application/admin/controller/Avatar.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\admin\model\Avatar as M;
+
+use cropper\Crop;
+
+class Avatar extends Base{
+
+    public function index(){
+        $avatar = new M();
+        $list = $avatar->index();
+        $this->assign('list',$list);
+        $this->assign('page',$list->render());
+        $this->assign('count',$list->total());
+        return $this->fetch();
+    }
+
+    public function add(){
+        if (request()->isPost()){
+
+        }
+        return $this->fetch('edit');
+    }
+
+    public function test(){
+        $crop = new Crop($_POST['avatar_src'], $_POST['avatar_data'], $_FILES['avatar_file']);
+
+        $response = array(
+            'state'  => 200,
+            'message' => $crop -> getMsg(),
+            'result' => $crop -> getResult()
+        );
+
+        echo json_encode($response);
+    }
+
+}
--- application/config.php
+++ application/config.php
@@ -146,7 +146,8 @@ return [
         '__HUI__'       => '/static/h-ui',
         '__LIB__'       => '/lib',
         '__JS__'        => '/static/js',
-        '__CSS__'       => '/static/css'
+        '__CSS__'       => '/static/css',
+        '__IMAGE__'       =>'/image'
     ],
     // 默认跳转页面对应的模板文件
 //    'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
