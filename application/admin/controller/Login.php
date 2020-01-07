0d2796e4ef04645397f8b96dd6da31fa1b259fd8
pengchangxi <1965487995@qq.com>
2020-01-07 14:31:35 +0800

    style: 代码格式化

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -7,9 +7,11 @@ use think\Controller;
 use think\captcha\Captcha;
 use think\Request;
 
-class Login extends Controller{
+class Login extends Controller
+{
 
-    public function index(){
+    public function index()
+    {
         return $this->fetch('/template/login');
     }
 
@@ -17,9 +19,10 @@ class Login extends Controller{
      * @return \think\Response
      * 生成验证码
      */
-    public function verify(){
-        $Verify = new Captcha();
-        $Verify->length   = 4;
+    public function verify()
+    {
+        $Verify         = new Captcha();
+        $Verify->length = 4;
         return $Verify->entry();
     }
 
@@ -28,7 +31,8 @@ class Login extends Controller{
      * @return bool
      * 核对验证码
      */
-    public function verifyCheck($code){
+    public function verifyCheck($code)
+    {
         $verify = new Captcha();
         return $verify->check($code);
     }
@@ -36,50 +40,52 @@ class Login extends Controller{
     /**
      * 验证登录
      */
-    public function checkLogin(){
-        $request = Request::instance();
-        $account = $request->param('account');
-        $password = $request->param('password');
+    public function checkLogin()
+    {
+        $request    = Request::instance();
+        $account    = $request->param('account');
+        $password   = $request->param('password');
         $verifyCode = $request->param('verify');
-        $admin=new Admins();
-        $verify=$this->verifyCheck($verifyCode);
-        if (!$verify){
-            $this->error('验证码错误！','/admin/login/index');
+        $admin      = new Admins();
+        $verify     = $this->verifyCheck($verifyCode);
+        if (!$verify) {
+            $this->error('验证码错误！', '/admin/login/index');
         }
-        $map = array(
-            'account' =>$account,
-            'status' =>1
+        $map      = array(
+            'account' => $account,
+            'status'  => 1
         );
-        $authInfo=$admin->lookup($map);
-        if (empty($authInfo)){
-            $this->error('帐号不存在或已禁用！','/admin/login/index');
-        }else{
-            if(md5($password.$authInfo['salt']) !=$authInfo['password']){
+        $authInfo = $admin->lookup($map);
+        if (empty($authInfo)) {
+            $this->error('帐号不存在或已禁用！', '/admin/login/index');
+        } else {
+            if (md5($password . $authInfo['salt']) != $authInfo['password']) {
                 $this->error('密码错误！');
             }
             session('adminInfo', $authInfo);
-            $data = array(
+            $data        = array(
                 'last_login_time' => time(),
-                'last_login_ip' => $request->ip()
+                'last_login_ip'   => $request->ip()
             );
             $where['id'] = $authInfo['id'];
-            $admin->edit($where,$data);
-            $jumpurl=session('?loginjumpurl') ? session('loginjumpurl') : '/admin/index/index';
+            $admin->edit($where, $data);
+            $jumpurl = session('?loginjumpurl') ? session('loginjumpurl') : '/admin/index/index';
 
-            $this->success('登录成功！',$jumpurl);
+            $this->success('登录成功！', $jumpurl);
         }
     }
 
     //退出
-    public function logout(){
-        if(session('?adminInfo')) {
+    public function logout()
+    {
+        if (session('?adminInfo')) {
             session('adminInfo', null);//清除session
-            if(isset($_GET['isajax'])){
-                $this->success('超时退出！','/admin/login/index');
+            if (isset($_GET['isajax'])) {
+                $this->success('超时退出！', '/admin/login/index');
             }
-            $this->success('退出成功！','/admin/login/index');
-        }else {
-            $this->success('已经退出！','/admin/login/index');
+            $this->success('退出成功！', '/admin/login/index');
+        } else {
+            $this->success('已经退出！', '/admin/login/index');
         }
     }
 
