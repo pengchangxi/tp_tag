54d23e3d623142562b4bf421f8971f24e9a07890
pengchangxi <1965487995@qq.com>
2020-01-10 19:32:26 +0800

    feat: 增加mysql语句

--- version/data/feature-mysql-change.sql
+++ version/data/feature-mysql-change.sql
@@ -1,6 +1,15 @@
 INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (100000, '禁用', unix_timestamp(now()), unix_timestamp(now()));
 INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (100001, '启用', unix_timestamp(now()), unix_timestamp(now()));
+
 INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (101000, '隐藏', unix_timestamp(now()), unix_timestamp(now()));
 INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (101001, '显示', unix_timestamp(now()), unix_timestamp(now()));
 
-INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (100000, '禁用', unix_timestamp(now()), unix_timestamp(now()));
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (102000, '隐藏', unix_timestamp(now()), unix_timestamp(now()));
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (100001, '显示', unix_timestamp(now()), unix_timestamp(now()));
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (103000, '普通菜单', unix_timestamp(now()), unix_timestamp(now()));
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (103001, '页面菜单', unix_timestamp(now()), unix_timestamp(now()));
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (104000, '不记录操作日志', unix_timestamp(now()), unix_timestamp(now()));
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (104001, '记录操作日志', unix_timestamp(now()), unix_timestamp(now()));
+
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (105000, '禁用', unix_timestamp(now()), unix_timestamp(now()));
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (105001, '启用', unix_timestamp(now()), unix_timestamp(now()));
