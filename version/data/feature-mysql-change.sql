bf95e1444af802d4e13e36b3852de0110fa19376
pengchangxi <1965487995@qq.com>
2020-01-10 19:17:43 +0800

    feat: 添加mysql数据

---
+++ version/data/feature-mysql-change.sql
@@ -0,0 +1,6 @@
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (100000, '禁用', unix_timestamp(now()), unix_timestamp(now()));
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (100001, '启用', unix_timestamp(now()), unix_timestamp(now()));
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (101000, '隐藏', unix_timestamp(now()), unix_timestamp(now()));
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (101001, '显示', unix_timestamp(now()), unix_timestamp(now()));
+
+INSERT INTO tp_dict(code, cname, create_time, update_time) VALUES (100000, '禁用', unix_timestamp(now()), unix_timestamp(now()));
