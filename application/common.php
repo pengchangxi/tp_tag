df6f43e9f4e600d70197b9e15209723700fab573
pengchangxi <1965487995@qq.com>
2020-03-20 14:22:10 +0800

    style: 命名规范化

--- application/common.php
+++ application/common.php
@@ -37,11 +37,9 @@ function random($length = 6, $chars = '123456789abcdefghijklmnpqrstuvwxyzABCDEFG
  * @param string $delimiter 数字和单位分隔符
  * @return string            格式化后的带单位的大小
  */
-function format_bytes($size, $delimiter = '')
+function formatBytes($size, $delimiter = '')
 {
     $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
     for ($i = 0; $size >= 1024 && $i < 5; $i++) $size /= 1024;
     return round($size, 2) . $delimiter . $units[$i];
 }
-
-
