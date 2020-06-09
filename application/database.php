045f01998781f800165cbe9bbc27536f55c47c4e
pengchangxi <1965487995@qq.com>
2020-06-09 09:19:08 +0800

    feat: 更改数据库

--- application/database.php
+++ application/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => 'rm-2ze68913kbh1779lsxo.mysql.rds.aliyuncs.com',
     // 数据库名
     'database'        => 'tp5',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'liudd',
     // 密码
-    'password'        => '',
+    'password'        => 'UD1FWUAEmrcLV1Po',
     // 端口
-    'hostport'        => '',
+    'hostport'        => '3306',
     // 连接dsn
     'dsn'             => '',
     // 数据库连接参数
